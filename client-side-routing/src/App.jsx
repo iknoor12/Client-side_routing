beae11b67c1cab1d5050ac3d2ef3dcd041862941
iknoor12 <imiknoor@gmail.com>
2026-02-04 09:40:07 +0530

    done with task

--- client-side-routing/src/App.jsx
+++ client-side-routing/src/App.jsx
@@ -1,15 +1,15 @@
 import { Routes, Route, Link } from "react-router-dom";
 
 function Home() {
-  return <h2 className="text-xl">Home Page</h2>;
+  return <h2 className="text-2xl">This is my Home Page</h2>;
 }
 
 function About() {
-  return <h2 className="text-xl">About Page</h2>;
+  return <h2 className="text-2xl">This is my About Page</h2>;
 }
 
 function Contact() {
-  return <h2 className="text-xl">Contact Page</h2>;
+  return <h2 className="text-2xl">This is my Contact Page</h2>;
 }
 
 function App() {
@@ -17,10 +17,10 @@ function App() {
     <div className="p-5 grid place-items-center">
 
       {/* Navigation */}
-      <nav className="flex gap-10 mb-10">
-        <Link to="/" className="text-blue-500">Home</Link>
-        <Link to="/about" className="text-blue-500">About</Link>
-        <Link to="/contact" className="text-blue-500">Contact</Link>
+      <nav className="flex gap-15 mb-10 mt-3">
+        <Link to="/" className="text-blue-500 text-3xl">Home</Link>
+        <Link to="/about" className="text-blue-500 text-3xl">About</Link>
+        <Link to="/contact" className="text-blue-500 text-3xl">Contact</Link>
       </nav>
 
       {/* Routes */}
