53c7f89abaa54484200d757223d705fc70cc6219
iknoor12 <imiknoor@gmail.com>
2026-02-04 09:32:11 +0530

    Add navigation and remove extra files

--- client-side-routing/src/App.jsx
+++ client-side-routing/src/App.jsx
@@ -1,27 +1,27 @@
 import { Routes, Route, Link } from "react-router-dom";
 
-// function Home() {
-//   return <h2 className="text-xl">Home Page</h2>;
-// }
+function Home() {
+  return <h2 className="text-xl">Home Page</h2>;
+}
 
-// function About() {
-//   return <h2 className="text-xl">About Page</h2>;
-// }
+function About() {
+  return <h2 className="text-xl">About Page</h2>;
+}
 
-// function Contact() {
-//   return <h2 className="text-xl">Contact Page</h2>;
-// }
+function Contact() {
+  return <h2 className="text-xl">Contact Page</h2>;
+}
 
 function App() {
   return (
     <div className="p-5 grid place-items-center">
 
       {/* Navigation */}
-      {/* <nav className="flex gap-10 mb-10">
+      <nav className="flex gap-10 mb-10">
         <Link to="/" className="text-blue-500">Home</Link>
         <Link to="/about" className="text-blue-500">About</Link>
         <Link to="/contact" className="text-blue-500">Contact</Link>
-      </nav> */}
+      </nav>
 
       {/* Routes */}
       <Routes>
